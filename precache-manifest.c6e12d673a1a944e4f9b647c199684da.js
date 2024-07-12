130bfaf5b992d76d0d4e022927aa3b5fecaef204
surachaikongsati <100983253+surachaikongsati@users.noreply.github.com>
2024-07-12 11:02:50 +0700

    Add files via upload

---
+++ precache-manifest.c6e12d673a1a944e4f9b647c199684da.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eea9eb30b3231566c3329d80510cc0b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "3d6d5f34dd1e311dbfc1",
+    "url": "/react-deploy/static/css/main.64f4ae10.chunk.css"
+  },
+  {
+    "revision": "9a46c827e618f3492a7d",
+    "url": "/react-deploy/static/js/2.f2a415c9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-deploy/static/js/2.f2a415c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6d5f34dd1e311dbfc1",
+    "url": "/react-deploy/static/js/main.28d604e0.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  },
+  {
+    "revision": "1f928ad1fe02182ab5241c1d0ade6549",
+    "url": "/react-deploy/static/media/001.1f928ad1.jpg"
+  },
+  {
+    "revision": "f38a3986deddde778dfa85362ad15c4b",
+    "url": "/react-deploy/static/media/002.f38a3986.jpg"
+  },
+  {
+    "revision": "04f42e415bcb8d9ecf53c1f645f8dc31",
+    "url": "/react-deploy/static/media/003.04f42e41.jpg"
+  },
+  {
+    "revision": "7a79fc83520c01ce8fa19531584393f7",
+    "url": "/react-deploy/static/media/004.7a79fc83.jpg"
+  },
+  {
+    "revision": "a20ebc914dffe639506271ee0e811dd1",
+    "url": "/react-deploy/static/media/005.a20ebc91.jpg"
+  },
+  {
+    "revision": "aef4d98aac5bfc49609d876ad03e9549",
+    "url": "/react-deploy/static/media/006.aef4d98a.jpg"
+  },
+  {
+    "revision": "824c47d988dbd9f8ac60ab0db4e5e585",
+    "url": "/react-deploy/static/media/007.824c47d9.jpg"
+  },
+  {
+    "revision": "bada55fcfa7271afae0b49cab2757eb6",
+    "url": "/react-deploy/static/media/008.bada55fc.jpg"
+  },
+  {
+    "revision": "19c91db4d0a4dc5aeb27b417aa04b8e3",
+    "url": "/react-deploy/static/media/009.19c91db4.jpg"
+  },
+  {
+    "revision": "2d5f4af96ab2a40f05e08713fd251ea1",
+    "url": "/react-deploy/static/media/010.2d5f4af9.jpg"
+  },
+  {
+    "revision": "6e6aa053baa41189c7fbf4126a49c840",
+    "url": "/react-deploy/static/media/011.6e6aa053.jpg"
+  },
+  {
+    "revision": "05a7cf3d2a19cc2765021d2c25439065",
+    "url": "/react-deploy/static/media/012.05a7cf3d.jpg"
+  },
+  {
+    "revision": "4cc2a528bfe785e882d1e350262c676a",
+    "url": "/react-deploy/static/media/013.4cc2a528.jpg"
+  },
+  {
+    "revision": "5ceb56edb54abf305f00f3be8df1057e",
+    "url": "/react-deploy/static/media/014.5ceb56ed.jpg"
+  },
+  {
+    "revision": "d0f6e163d263e3951fdafc5c05903e22",
+    "url": "/react-deploy/static/media/015.d0f6e163.jpg"
+  },
+  {
+    "revision": "215bbd6d7f8e992e11e6d9fc6386fa0b",
+    "url": "/react-deploy/static/media/016.215bbd6d.jpg"
+  },
+  {
+    "revision": "12c3f68f46eaabe4cae07bd53020ba3a",
+    "url": "/react-deploy/static/media/017.12c3f68f.jpg"
+  },
+  {
+    "revision": "97bc4deb180906dcd1f830ca36d1b350",
+    "url": "/react-deploy/static/media/018.97bc4deb.jpg"
+  },
+  {
+    "revision": "eece4dc1607f52f204bfa951477f9ee2",
+    "url": "/react-deploy/static/media/019.eece4dc1.jpg"
+  },
+  {
+    "revision": "6fc04df44b795b4a33e060a34801ed1a",
+    "url": "/react-deploy/static/media/020.6fc04df4.jpg"
+  },
+  {
+    "revision": "a7114a74b8aca9732291d02b7291db1d",
+    "url": "/react-deploy/static/media/021.a7114a74.jpg"
+  },
+  {
+    "revision": "b0cdaa56e2e86c678693610389ca6c14",
+    "url": "/react-deploy/static/media/022.b0cdaa56.jpg"
+  },
+  {
+    "revision": "039f039636725cd18a5042bf4c5a1766",
+    "url": "/react-deploy/static/media/023.039f0396.jpg"
+  },
+  {
+    "revision": "f5e15c3fb2683a81ddafb0e41b6776bd",
+    "url": "/react-deploy/static/media/024.f5e15c3f.jpg"
+  },
+  {
+    "revision": "ab66088fc9b8051d0ed8470e82173f84",
+    "url": "/react-deploy/static/media/025.ab66088f.jpg"
+  },
+  {
+    "revision": "467c666bbf717a8e59599a49c2a83bcb",
+    "url": "/react-deploy/static/media/026.467c666b.jpg"
+  },
+  {
+    "revision": "0709fb00267b460c39552b58bab89f08",
+    "url": "/react-deploy/static/media/027.0709fb00.jpg"
+  },
+  {
+    "revision": "7996a776ac220a8e014c904bb7475ba7",
+    "url": "/react-deploy/static/media/028.7996a776.jpg"
+  },
+  {
+    "revision": "355d184e7c57df9b5eb386bc78840207",
+    "url": "/react-deploy/static/media/029.355d184e.jpg"
+  },
+  {
+    "revision": "ffb480f623d9171ca5dcd53718eec293",
+    "url": "/react-deploy/static/media/030.ffb480f6.jpg"
+  },
+  {
+    "revision": "0fc98fce1753b5caeb46d7e312ff33b5",
+    "url": "/react-deploy/static/media/031.0fc98fce.jpg"
+  },
+  {
+    "revision": "85d2a2a3dcc134b90fbf09fef2d46fcf",
+    "url": "/react-deploy/static/media/032.85d2a2a3.jpg"
+  },
+  {
+    "revision": "bab30693219e0e636c34cc47460d2f16",
+    "url": "/react-deploy/static/media/aeh.bab30693.jpg"
+  },
+  {
+    "revision": "136bd3bc24b67c542780eac93d3e7924",
+    "url": "/react-deploy/static/media/mom.136bd3bc.jpg"
+  }
+]);
